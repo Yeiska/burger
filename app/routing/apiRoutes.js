7ae2de204be907be3b0c12b648fe7c8cb5456d6b
Yeiska <yeiska83@yahoo.com>
2018-08-16 23:19:52 -0400

    Api working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,38 +1,43 @@
 //Data
-var express = require("express");
-var friends = require("../data/friends.js");
-
-var app = express();
+var friends = require("../data/friends");
+var path = require("path");
 //Routing
-module.exports = function(app){
-    app.get("/api/friends", function(req, res){
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+
+    app.post("/api/friends", function (req, res) {
+        var newfriendScore = req.body.scores;
+        var scoresArray = [];
+        var friendsCount = 0;
+        var match = 0;
+
+        for (var i = 0; i < scoresArray.length; i++) {
+            var diff = 0;
+            for (var j = 0; j < newfriendScore.length; i++) {
+                diff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newfriendScore[j])));
+            }
+            scoresArray.push(diff);
+        }
+
+        //Find best match after all friends comprare
+        for (var i = 0; i < scoresArray.length; i++) {
+            if (scoresArray[i] <= scoresArray[match]) {
+                match = i;
+            }
+        }
+        //return match data
+        var bestMatch = friends[match];
+        res.json(bestMatch);
+        //push new best friends into array
+        friends.push(req.body);
+    })
 };
-
-app.post("/api/friends", function(req, res){
-    var newfriendScore = req.body.scores;
-    var scoresArray = [];
-    var friendsCount = 0;
-    var match = 0;
-
-    for (var i = 0; i < scoresArray.length; i++){
-        var diff = 0;
-        for (var j = 0; j < newfriendScore.length; i++){
-            diff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newfriendScore[j])));
-        }
-        scoresArray.push(diff);
-    }
-
-    //Find best match after all friends comprare
-    for (var i = 0; i < scoresArray.length; i++){
-        if(scoresArray[i] <= scoresArray[match]){
-            match = i;
-        }
-    }
-    //return match data
-    var bestMatch = friends[match];
-    res.json(bestMatch);
-    //push new best friends into array
-    friends.push(req.body);
-})
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require("path");
 
 //Express app
 var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+//var bodyParser = require("body-parser");
+
 var app = express();
 
 
@@ -16,18 +16,11 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json());
-//app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 //Routes
-require("./app/routing/apiRoutes.js");
-require("./app/routing/htmlRoutes.js");
-
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "/app/public/home.html"));
-});
-app.get("/survey", function(req, res){
-    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
-});
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 //Listener
 app.listen(PORT, function(){
