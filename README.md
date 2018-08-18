eeb6e5eeb5a0768f55c2e639a590e1da8fe2da6a
Yeiska <yeiska83@yahoo.com>
2018-08-17 21:36:30 -0400

    add readme description

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # FriendFinder
+* Create a Friend Finder app (essentially a dating app but for friends) using Node and Express servers. The user enters the page that leads to a survey. When the user submits the survey, they are matched with a "friend" that has the closest match to their personality.
+
+## Screen Shots
+### Home page
+![Friend Finder home page Image](img/home.png)
+### api Fiends data
+![Api Friend Data Image](img/apiFiends.png)
+### Survey page
+![Survey Image](img/survey.png)
+### Match
+![Match Image](img/match.png)
+
+## Technologies used
+* Node.js
+* body-parager NPM Package - https://www.npmjs.com/package/inquirer
+* express NPM Package - https://www.npmjs.com/package/express
+* path NPM Package - https://www.npmjs.com/package/path
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,52 @@
 //Data
 var survey = [
     {
-        "name" :"",
-        "photo" :"",
-        "scores" :""
+        name: 'Juana artiles',
+        profilePic: 'https://www.google.com/imgres?imgurl=http%3A%2F%2Fimg.brothersoft.com%2Fscreenshots%2Fsoftimage%2Fp%2Fplaycolors_flowers_screensaver-95115-1.jpeg&imgrefurl=http%3A%2F%2Fwallpapersofflowers.blogspot.com%2F2012%2F01%2Ffree-images-of-flowers_7745.html&docid=Z38zfAdObzg9dM&tbnid=6UD65xvBGWoQDM%3A&vet=10ahUKEwim5a2As_XcAhXvg-AKHX41BcIQMwhIKAgwCA..i&w=1365&h=1024&bih=662&biw=1366&q=flowers%20img&ved=0ahUKEwim5a2As_XcAhXvg-AKHX41BcIQMwhIKAgwCA&iact=mrc&uact=8',
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: 'Sury mendes',
+        profilePic: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.all-free-download.com%2Fimages%2Fgraphiclarge%2Ftulip_flowers_highdefinition_picture_03_166859.jpg&imgrefurl=https%3A%2F%2Fall-free-download.com%2Ffree-photos%2Fflower-images.html&docid=whf96_3lOjb5LM&tbnid=cw_Dxw5R3hJhiM%3A&vet=10ahUKEwim5a2As_XcAhXvg-AKHX41BcIQMwhJKAkwCQ..i&w=462&h=600&bih=662&biw=1366&q=flowers%20img&ved=0ahUKEwim5a2As_XcAhXvg-AKHX41BcIQMwhJKAkwCQ&iact=mrc&uact=8',
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: 'Tomasa Hernandez',
+        profilePic: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F236x%2Fd0%2F22%2Fc6%2Fd022c692de561a4c138a01258540a717--vintage-clip-vintage-art.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F435934438904515154%2F&docid=yHidfTm6fAcRuM&tbnid=7vA3h-8hQrh17M%3A&vet=10ahUKEwim5a2As_XcAhXvg-AKHX41BcIQMwhBKAEwAQ..i&w=236&h=317&bih=662&biw=1366&q=flowers%20img&ved=0ahUKEwim5a2As_XcAhXvg-AKHX41BcIQMwhBKAEwAQ&iact=mrc&uact=8',
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
     }
 ];
 
