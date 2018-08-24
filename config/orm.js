90701e7fa2d7791a318c78fe4ece3a65f4335c7e
Yeiska <yeiska83@yahoo.com>
2018-08-24 00:22:21 -0400

    Fix heroku connection and better html

--- config/orm.js
+++ config/orm.js
@@ -10,6 +10,19 @@ function printBurger(burger){
     return arr.toString();
 }
 
+function objToSql(ob) {
+    var arr = [];
+  
+    for (var key in ob) {
+      if (Object.hasOwnProperty.call(ob, key)) {
+        arr.push(key + "=" + ob[key]);
+      }
+    }
+  
+    return arr.toString();
+  }
+  
+
 var orm = {
     selectAll: function(tableInput, cb){
         var queryString = " SELECT * FROM " + tableInput + ";" ;
